40b8427dbf0456e04aedf771785dcf8c306ac910
Sash Zats <sash@zats.io>
2026-02-10 10:10:40 -0500

    Revert "Add diagnostics for sandboxed process enumeration"
    This reverts commit 6db919f124d14b59c26ae65456accbe4ffceacdd.

--- Mouth/Engine/ProcessSnapshot.swift
+++ Mouth/Engine/ProcessSnapshot.swift
@@ -2,22 +2,6 @@ import Foundation
 import Darwin
 
 struct ProcessSnapshot {
-    struct CaptureDiagnostics {
-        let listAllPidsInitial: Int32
-        let listAllPidsFilled: Int32
-        let pidInfoOK: Int
-        let pidInfoFailed: Int
-        let pidPathFailed: Int
-
-        // errno is process-global; we capture it right after the syscall-like calls for hinting.
-        let errnoAfterInitial: Int32
-        let errnoAfterFilled: Int32
-
-        var summary: String {
-            "pidsInitial=\(listAllPidsInitial) errnoInitial=\(errnoAfterInitial) pidsFilled=\(listAllPidsFilled) errnoFilled=\(errnoAfterFilled) pidInfoOK=\(pidInfoOK) pidInfoFailed=\(pidInfoFailed) pidPathFailed=\(pidPathFailed)"
-        }
-    }
-
     struct ProcessInfo {
         let pid: pid_t
         let ppid: pid_t
@@ -28,44 +12,16 @@ struct ProcessSnapshot {
     let byPid: [pid_t: ProcessInfo]
 
     static func capture() -> ProcessSnapshot {
-        captureWithDiagnostics().snapshot
-    }
-
-    static func captureWithDiagnostics() -> (snapshot: ProcessSnapshot, diagnostics: CaptureDiagnostics) {
-        errno = 0
-        let initial = proc_listallpids(nil, 0)
-        let errnoAfterInitial = errno
-
-        if initial <= 0 {
-            let diag = CaptureDiagnostics(
-                listAllPidsInitial: initial,
-                listAllPidsFilled: 0,
-                pidInfoOK: 0,
-                pidInfoFailed: 0,
-                pidPathFailed: 0,
-                errnoAfterInitial: errnoAfterInitial,
-                errnoAfterFilled: 0
-            )
-            return (ProcessSnapshot(byPid: [:]), diag)
+        let count = proc_listallpids(nil, 0)
+        if count <= 0 {
+            return ProcessSnapshot(byPid: [:])
         }
 
-        var pids = Array<pid_t>(repeating: 0, count: Int(initial))
+        var pids = Array<pid_t>(repeating: 0, count: Int(count))
         let bytes = Int32(pids.count * MemoryLayout<pid_t>.stride)
-        errno = 0
         let filled = proc_listallpids(&pids, bytes)
-        let errnoAfterFilled = errno
-
         if filled <= 0 {
-            let diag = CaptureDiagnostics(
-                listAllPidsInitial: initial,
-                listAllPidsFilled: filled,
-                pidInfoOK: 0,
-                pidInfoFailed: 0,
-                pidPathFailed: 0,
-                errnoAfterInitial: errnoAfterInitial,
-                errnoAfterFilled: errnoAfterFilled
-            )
-            return (ProcessSnapshot(byPid: [:]), diag)
+            return ProcessSnapshot(byPid: [:])
         }
 
         pids = pids.prefix(Int(filled)).filter { $0 > 0 }
@@ -73,19 +29,13 @@ struct ProcessSnapshot {
         var map: [pid_t: ProcessInfo] = [:]
         map.reserveCapacity(pids.count)
 
-        var pidInfoOK = 0
-        var pidInfoFailed = 0
-        var pidPathFailed = 0
-
         for pid in pids {
             var info = proc_bsdinfo()
             let infoSize = Int32(MemoryLayout<proc_bsdinfo>.stride)
             let ret = proc_pidinfo(pid, PROC_PIDTBSDINFO, 0, &info, infoSize)
             if ret != infoSize {
-                pidInfoFailed += 1
                 continue
             }
-            pidInfoOK += 1
 
             let name = withUnsafePointer(to: &info.pbi_name) {
                 $0.withMemoryRebound(to: CChar.self, capacity: Int(MAXCOMLEN)) { ptr in
@@ -94,22 +44,11 @@ struct ProcessSnapshot {
             }
 
             let path = ProcessSnapshot.pidPath(pid)
-            if path == nil { pidPathFailed += 1 }
 
             map[pid] = ProcessInfo(pid: pid, ppid: pid_t(info.pbi_ppid), name: name, path: path)
         }
 
-        let diag = CaptureDiagnostics(
-            listAllPidsInitial: initial,
-            listAllPidsFilled: filled,
-            pidInfoOK: pidInfoOK,
-            pidInfoFailed: pidInfoFailed,
-            pidPathFailed: pidPathFailed,
-            errnoAfterInitial: errnoAfterInitial,
-            errnoAfterFilled: errnoAfterFilled
-        )
-
-        return (ProcessSnapshot(byPid: map), diag)
+        return ProcessSnapshot(byPid: map)
     }
 
     func isDescendant(_ pid: pid_t, of ancestor: pid_t) -> Bool {
